ea84d387078c824792a5384ecc6313bf77041fd0
ronald otwala <chigaji@gmail.com>
2024-01-18 11:49:07 +0900

    removed unnecessary fields

--- models/expense.go
+++ models/expense.go
@@ -1,16 +1,13 @@
 package models
 
 import (
-	"time"
-
 	"gorm.io/gorm"
 )
 
 type Expense struct {
 	gorm.Model
-	UserID      uint      `json:"user_id"`
-	Amount      float64   `json:"amount"`
-	Caterory    string    `json:"category"`
-	Description string    `json:"description"`
-	Date        time.Time `json:"date"`
+	UserID      uint    `json:"user_id"`
+	Amount      float64 `json:"amount"`
+	Caterory    string  `json:"category"`
+	Description string  `json:"description"`
 }
